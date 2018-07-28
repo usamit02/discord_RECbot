1616bd765d96b3c1961039495db8c7c7ea1bae02
usamit02 <usamit02@gmail.com>
2018-07-28 20:10:31 +0900

    非同期失敗

--- conv.js
+++ conv.js
@@ -3,31 +3,42 @@ const ffmpeg = require("fluent-ffmpeg");
 const request = require("request");
 const fs = require("fs");
 
-ffmpeg()
-  .input('./rec/clife.pcm')
-  .inputOptions(['-ac 2', '-ar 48000'])
-  .inputFormat('s16le')
-  .output('./rec/demo1.mp3')
-  .outputOptions(['-ab 64k'])
-  .on('end', function () {
-    console.log('file has been converted succesfully');
-    var formData = {
-      "mp3": fs.createReadStream("./rec/demo1.mp3"),
-      "person": "suzuki"
-    }
-    var options = {
-      url: "http://localhost/enter.php", formData: formData
-    }
-    request.post(options, function (err, response, body) {
-      if (!err && response.statusCode == 200) {
-        console.log(JSON.parse(body).msg);
-      } else {
-        console.log(err);
-      }
-    })
-  })
+var toDay = new Date;
+//toDay.setDate(toDay.getDate() - 1);
+const paths = String(toDay.getFullYear()) + "0" + String(toDay.getMonth() + 1).slice(-2) + '/' + String(toDay.getDate());
+const pathl = './rec/' + paths + '/';
+try {
+  var files = fs.readdirSync(pathl);
+  for (var i in files) {
+    var file = files[i].split('.');
+    if (file[1] == "pcm") {
+      ffmpeg()
+        .input(pathl + files[i])
+        .inputOptions(['-ac 2', '-ar 48000'])
+        .inputFormat('s16le')
+        .output(pathl + file[0] + '.mp3')
+        .outputOptions(['-ab 48k'])
+        .on('end', function () {
+          console.log('file has been converted succesfully');
+          var fd = {
+            'mp3': fs.createReadStream(pathl + file[0] + '.mp3'),
+            'path': paths
+          }//http://accessblog.s1001.xrea.com/enter.php
+          request.post({ url: "http://localhost/enter.php", formData: fd }, function (err, response, body) {
+            if (!err && response.statusCode == 200) {
+              console.log(JSON.parse(body).msg);
+            } else {
+              console.log(err);
+            }
+          })
+        })
 
-  .on('error', function (err) {
-    console.log('an error happened: ' + err.message);
-  })
-  .run()
+        .on('error', function (err) {
+          console.log('ffmpeg error: ' + err.message);
+        })
+        .run()
+    }
+  }
+} catch (e) {
+  console.log(e);
+}
