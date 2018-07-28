c2638e501ee075bfc8077b3089cc5e14ae09d7dc
usamit02 <usamit02@gmail.com>
2018-07-28 21:58:59 +0900

    即時関数成功

--- conv.js
+++ conv.js
@@ -7,37 +7,61 @@ var toDay = new Date;
 //toDay.setDate(toDay.getDate() - 1);
 const paths = String(toDay.getFullYear()) + "0" + String(toDay.getMonth() + 1).slice(-2) + '/' + String(toDay.getDate());
 const pathl = './rec/' + paths + '/';
+
+var conv = function (file) {
+  var na = file.split('.');
+  return new Promise(function (resolve, reject) {
+    ffmpeg()
+      .input(pathl + file)
+      .inputOptions(['-ac 2', '-ar 48000'])
+      .inputFormat('s16le')
+      .output(pathl + na + '.mp3')
+      .outputOptions(['-ab 48k'])
+      .run()
+      .on('end', function () {
+        resolve(true);
+      })
+      .on('error', function () {
+        reject(false);
+      })
+  })
+}
+
+function send(file) {
+
+}
 try {
   var files = fs.readdirSync(pathl);
   for (var i in files) {
-    var file = files[i].split('.');
-    if (file[1] == "pcm") {
-      ffmpeg()
-        .input(pathl + files[i])
-        .inputOptions(['-ac 2', '-ar 48000'])
-        .inputFormat('s16le')
-        .output(pathl + file[0] + '.mp3')
-        .outputOptions(['-ab 48k'])
-        .on('end', function () {
-          console.log('file has been converted succesfully');
-          var fd = {
-            'mp3': fs.createReadStream(pathl + file[0] + '.mp3'),
-            'path': paths
-          }//http://accessblog.s1001.xrea.com/enter.php
-          request.post({ url: "http://localhost/enter.php", formData: fd }, function (err, response, body) {
-            if (!err && response.statusCode == 200) {
-              console.log(JSON.parse(body).msg);
-            } else {
-              console.log(err);
-            }
+    (function (i) {
+      var file = files[i].split('.');
+      if (file[1] == "pcm") {
+        ffmpeg()
+          .input(pathl + files[i])
+          .inputOptions(['-ac 2', '-ar 48000'])
+          .inputFormat('s16le')
+          .output(pathl + file[0] + '.mp3')
+          .outputOptions(['-ab 48k'])
+          .on('end', function () {
+            console.log('file has been converted succesfully');
+            var fd = {
+              'mp3': fs.createReadStream(pathl + file[0] + '.mp3'),
+              'path': paths
+            }//http://accessblog.s1001.xrea.com/enter.php
+            request.post({ url: "http://localhost/enter.php", formData: fd }, function (err, response, body) {
+              if (!err && response.statusCode == 200) {
+                console.log(JSON.parse(body).msg);
+              } else {
+                console.log(err);
+              }
+            })
           })
-        })
-
-        .on('error', function (err) {
-          console.log('ffmpeg error: ' + err.message);
-        })
-        .run()
-    }
+          .on('error', function (err) {
+            console.log('ffmpeg error: ' + err.message);
+          })
+          .run()
+      }
+    })(i);
   }
 } catch (e) {
   console.log(e);
