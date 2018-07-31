20e5835cf2599621177587c01a9dbd63d1f8b901
usamit02 <usamit02@gmail.com>
2018-07-31 10:35:29 +0900

    テストボット

--- rec.js
+++ rec.js
@@ -1,39 +1,67 @@
 const Discord = require("discord.js");
 const fs = require('fs-extra');
-const client = new Discord.Client();
 const mysql = require('mysql');
 
-const mainChannelId = "472344675676585985";
-const subChannelId = "";
-const radioChannelId = "472264910223704074";
-
-const db = mysql.createConnection({
+const mainChannelId = "472344675676585985";//メインラウンジ
+const subChannelId = "";//放送内容共有メモ
+const radioChannelId = "472264910223704074";//ブロギルラジオ
+const maxRECmin = 30;//最大録音可能時間（分）
+const dbString = {
   host: 'localhost',
   user: 'root',
   password: 'kx125l1',
   database: 'recbot',
   supportBigNumbers: true,
   bigNumberStrings: true
-});
-var caster = [];
-class _program {
+}
+const client = new Discord.Client();
+var caster = [];//登録済パーソナリティ
+class _caster {
+  constructor(id) {
+    this.id = id;
+    this.program = [];
+  }
+  addProgram() {
+    this.program[this.program.length] = new _program;
+  }
+  addSummary(author, txt) {
+    if (this.program.length) {
+      return this.program[this.program.length - 1].addSummary(author, txt);
+    }
+    return false;
+  }
+  getRECtime() {
+    if (this.program.length) {
+      return this.program[this.program.length - 1].end - this.program[this.program.length - 1].start;
+    }
+    return false;
+  }
+}
+class _program {//ラジオ番組}
   constructor() {
     this.start = new Date();
     this.end = new Date();
     this.summary = [];
+    this.fault = false;
+    this.sync = false;
+  }
+  addSummary(author, txt) {
+    var l = this.summary.length;
+    this.summary[l] = new _summary(l, author, txt);
+    return l;
   }
 }
-class _caster {
-  constructor(id) {
+class _summary {//まとめ
+  constructor(id, author, txt) {
     this.id = id;
-    this.count = 0;
-    this.program = {};
-  }
-  addProgram() {
-    this.program[this.count] = new _program;
+    this.upd = new Date();
+    this.author = author;
+    this.txt = txt;
   }
 }
-var recording = "";
+//初期設定
+var recording = ""; var recPath = ""; var recFile = "";
+var db = mysql.createConnection(dbString);
 db.connect((err) => {
   if (err) throw err;
   db.query("SELECT id FROM t01caster WHERE stop = 0;", (err, row) => {
@@ -41,88 +69,134 @@ db.connect((err) => {
     for (var i = 0; i < row.length; i++) {
       caster[i] = new _caster(row[i].id);
     }
-    //Object.assign(caster, row);
+    db.end();
   });
 });
-
-client.on('message', msg => {
-  if (msg.content.startsWith('rec')) {
-    let [command, ...channelName] = msg.content.split(" ");
-    if (!msg.guild) {
-      return msg.reply('no private service is available in your area at the moment. Please contact a service representative for more details.');
-    }
-    const voiceChannel = msg.guild.channels.find("name", "General");//channelName.join(" "));
-    //console.log(voiceChannel.id);
-    if (!voiceChannel || voiceChannel.type !== 'voice') {
-      return msg.reply(`I couldn't find the channel ${channelName}. Can you spell?`);
-    }
-    voiceChannel.join()
-      .then(conn => {
-        msg.reply('ready!');
-        var receiver = conn.createReceiver();
-        conn.on('speaking', (user, speaking) => {
-          if (speaking) {
-            var outputStream = fs.createWriteStream(`./rec/record-3.pcm`, { 'flags': 'a' });// pipe our audio data into the file stream
-            msg.channel.sendMessage(`RECroding to ${user}`);// this creates a 16-bit signed PCM, stereo 48KHz PCM stream.
-            const audioStream = receiver.createPCMStream(user);// create an output stream so we can dump our data in a file
-            audioStream.pipe(outputStream);
-            //outputStream.on("data", console.log);
-            audioStream.on('end', () => {// when the stream ends (the user stopped talking) tell the user
-              msg.channel.sendMessage(`RECroding stop ${user}`);
-              outputStream.end();
-            });
-          }
-        });
-      })
-      .catch(console.log);
+function saveSQL(recorder) {//配信サーバー同期用SQLファイル作成
+  var sql = ""; recording = ""; recFile = "";
+  function escTxt(txt) {
+    return txt
+      .replace("'", '"')
+      .replace("`", "");
   }
-  if (msg.content.startsWith('stop')) {
-    let [command, ...channelName] = msg.content.split(" ");
-    let voiceChannel = msg.guild.channels.find("name", channelName.join(" "));
-    voiceChannel.leave();
+  function dateFormat(date) {//MySQL用日付文字列作成'yyyy-M-d H:m:s'
+    var y = date.getFullYear();
+    var m = date.getMonth() + 1;
+    var d = date.getDate();
+    var h = date.getHours();
+    var min = date.getMinutes();
+    var sec = date.getSeconds();
+    return "'" + y + "-" + m + "-" + d + " " + h + ":" + min + ":" + sec + "'";
   }
-  if (msg.isMemberMentioned(client.user) && msg.member.voiceChannel) {
-    msg.member.voiceChannel.join().then(connection => {
-      const dispatcher = connection.playFile('./rec/r3.ogg');
-      dispatcher.on('end', reason => {
-        connection.disconnect();
+  for (var i = 0; i < recorder.program.length; i++) {
+    if (!recorder.program[i].fault && !recorder.program[i].sync) {
+      sql += "INSERT INTO t02program (cid,id,start,end) VALUES (";
+      sql += recorder.id + "," + i + "," + dateFormat(recorder.program[i].start) + ","
+        + dateFormat(recorder.program[i].end) + ");\r\n";
+      var summary = recorder.program[i].summary
+      summary.sort(function (a, b) {
+        if (a.author < b.author) return -1;
+        if (a.author > b.author) return 1;
+        if (a.id < b.id) return -1;
+        if (a.id > b.id) return 1;
+        return 0
       });
-    })
-      .catch(console.log);
+      summary.push({ id: 0 });
+      var txt = "";
+      for (var j = 0; j < summary.length - 1; j++) {
+        if (summary[j].author === summary[j + 1].author) {
+          txt += escTxt(summary[j].txt) + "<br>";
+        } else {
+          txt += escTxt(summary[j].txt);
+          txt = txt.length > 1500 ? txt.slice(0, 1500) : txt;
+          sql += "INSERT INTO t12summary (cid,pid,id,txt) VALUES (";
+          sql += recorder.id + "," + i + "," + summary[j].author + ",'" + txt + "');\r\n";
+          txt = "";
+        }
+      }
+    }
+  }
+  fs.appendFile(recPath + "/sync.sql", sql, err => {
+    if (err) { throw err; }
+    for (var i = 0; i < recorder.program.length; i++) {
+      recorder.program[i].sync = true;
+      delete recorder.program[i].summary;
+    }
+  });
+}
+client.on('message', msg => {
+  if (msg.isMemberMentioned(client.user)) {
+    if (recording) {//録音中にメンション飛ばすと番組中投稿保存モードになる
+      let i = msg.content.indexOf(">");
+      var txt = i > 1 ? msg.content.substr(i + 2) : "";
+      var recorder = caster.filter(i => { if (i.id === recording) return true; });
+      recorder[0].addSummary(msg.author.id, txt);
+      msg.channel.send("これからの投稿はメモっとくよ、" + msg.author.username + "。");
+    } else {//録音中しか反応しない
+      msg.channel.send("ZZZ...");
+    }
     return;
+  } else {
+    if (recording && (msg.channel.id === mainChannelId || msg.channel.id === subChannelId)) {
+      var recorder = caster.filter(i => { if (i.id === recording) return true; });
+      var author = recorder[0].program[recorder[0].program.length - 1].summary.filter(i => { if (i.author === msg.author.id) return true; });
+      if (author.length) {//投稿者名義の書き込みが既存していれば番組中の投稿は全て記録される
+        recorder[0].addSummary(msg.author.id, msg.content);
+      }
+    }
   }
 });
-client.on('voiceStateUpdate', (oldMember, newMember) => {
-  var recorder = caster.filter(function (i) { if (i.id == newMember.id) return true; });
+client.on('voiceStateUpdate', (oldMember, newMember) => {//ボイスチャンネルに誰か出入りしたとき発火
+  var recorder = caster.filter(i => { if (i.id === newMember.id) return true; });
   if (recorder.length) {//新しくボイスチャンネルに入った人がキャスター一覧にあったとき
     if (oldMember.voiceChannelID !== newMember.voiceChannelID && newMember.voiceChannelID === radioChannelId && !newMember.mute && !recording) {
       newMember.voiceChannel.join()//ブロギルラジオチャンネルが録音していなくてミュート解除状態で入ったらボット録音開始
         .then(conn => {
           var receiver = conn.createReceiver();
           recording = newMember.id;
-          recorder[0].count++;
+          recorder[0].addProgram();
           let toDay = new Date;
-          let path = './rec/' + String(toDay.getFullYear()) + "0" + String(toDay.getMonth() + 1).slice(-2) + '/' + String(toDay.getDate());
-          fs.mkdirs(path);
-          var recfile = path + '/' + recording + '_' + recorder[0].count + '.pcm';
-          newMember.guild.channels.get(mainChannelId).send(recfile + "録音はじめ、" + newMember.displayName + "ガンバ！");
+          recPath = './rec/' + String(toDay.getFullYear()) + "0" + String(toDay.getMonth() + 1).slice(-2) + '/' + String(toDay.getDate());
+          fs.mkdirs(recPath);
+          recFile = recPath + '/' + recording + '_' + (recorder[0].program.length) + '.pcm';
+          var msg = newMember.guild.channels.get(mainChannelId);
+          msg.send("録音はじめ、" + newMember.displayName + "ガンバ！");
 
           conn.on('speaking', (user, speaking) => {
             if (speaking) {
-              var outputStream = fs.createWriteStream(recfile, { 'flags': 'a' });// pipe our audio data into the file stream
-              const audioStream = receiver.createPCMStream(user);// create an output stream so we can dump our data in a file
+              var outputStream = fs.createWriteStream(recFile, { 'flags': 'a' });// pipe our audio data into the file stream
+              const audioStream = receiver.createOpusStream(user);// create an output stream so we can dump our data in a file
               audioStream.pipe(outputStream);
               audioStream.on('end', () => {
                 outputStream.end();
+                recorder[0].program[recorder[0].program.length - 1].end = new Date;//番組終了時刻を記録
+                if (recorder[0].getRECtime() > maxRECmin * 60000) {//maxRECmin以上経過で強制終了
+                  newMember.voiceChannel.leave();
+                  msg.send("長いよ" + newMember.displayName + "、" + maxRECmin + "分が限界。");
+                  saveSQL(recorder[0]);
+                }
               });
             }
           });
         })
         .catch(console.log);
-    } else if (oldMember.voiceChannelID === radioChannelId && oldMember.id === recording) {
-      oldMember.voiceChannel.leave();
-      oldMember.guild.channels.get(mainChannelId).send("録音おわり、" + newMember.displayName + "乙");
-      recording = "";
+    } else if (oldMember.voiceChannelID === radioChannelId && oldMember.id === recording) {//キャスターがブロギルラジオチャンネルから抜けたとき
+      oldMember.voiceChannel.leave();//RECbotも抜ける
+      let rectime = recorder[0].getRECtime();
+      let min = Math.floor(rectime / 60000); let sec = Math.floor((rectime - min * 60000) / 1000);
+      var msg = oldMember.guild.channels.get(mainChannelId);
+      if (1) {
+        msg.send("録音おわり" + min + "分" + sec + "秒、" + newMember.displayName + "乙");
+      } else {//録音時間が１分以下のときは記録しない
+        msg.send("1分以上しゃべってくれないと困るなー、たった" + sec + "秒だよ" + newMember.displayName + "。");
+        recorder[0].program[recorder[0].program.length - 1].fault = true;
+        if (recFile) {
+          fs.unlink(recFile, function (err) {
+            if (err) { console.log(err); }
+          });
+        }
+      }
+      saveSQL(recorder[0]);
     }
   }
 })
@@ -130,5 +204,5 @@ client.on('voiceStateUpdate', (oldMember, newMember) => {
 client.login('NDY1NzQ1ODc4NDk0MjE2MjA0.DiR_Mw.Zcr7SkOwKq1MaJU1u4wrFRx2j4E');
 
 client.on('ready', () => {
-  console.log('ready!');
+  console.log('RECbot on ready!');
 });
